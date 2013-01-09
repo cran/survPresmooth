92d5d8876dca47a5fe00e34a172c43b7077a17fc
Ignacio Lopez de Ullibarri <ilu@udc.es>
2013-01-09 00:00:00 +0000

    version 1.1-5

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,3 @@
-.First.lib <- function(lib, pkg){
-	library.dynam("survPresmooth", pkg, lib)
-}
-
-.Last.lib <- function (libpath){
-  library.dynam.unload("survPresmooth", libpath=libpath) 
+.onUnload <- function (libpath){
+  library.dynam.unload("survPresmooth", libpath) 
 }
